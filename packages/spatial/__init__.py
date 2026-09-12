f98ac0ce2afb92ba9ffaf8c9ae47c8be425240a4
Marcelo Roldão Matos <marceloroldao@gmail.com>
2026-09-12 02:21:58 -0300

    spatial: export region catalog and entity index

--- packages/spatial/__init__.py
+++ packages/spatial/__init__.py
@@ -1,3 +1,11 @@
+from .region_entity_index import RegionEntityIndex
+from .regions import Region, RegionCatalog
 from .resolver import InterestConfig, SpatialResolver
 
-__all__ = ["InterestConfig", "SpatialResolver"]
+__all__ = [
+    "InterestConfig",
+    "SpatialResolver",
+    "Region",
+    "RegionCatalog",
+    "RegionEntityIndex",
+]
