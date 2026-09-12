76c0f9f2339069af1be47d375717cb13ef829027
Marcelo Roldão Matos <marceloroldao@gmail.com>
2026-09-12 09:31:20 -0300

    feat: export mutation gate policy types

--- packages/spatial/__init__.py
+++ packages/spatial/__init__.py
@@ -1,5 +1,6 @@
 from .cold_mutation import ColdEntityMutator, ColdMutationError
 from .cold_store import ColdRegionCandidateCache, FileRegionColdStore, externalize_world_entities
+from .mutation_gate import MutationDecision, MutationGate, MutationPrincipal
 from .region_entity_index import RegionEntityIndex
 from .region_spatial_grid import RegionLookupResult, RegionSpatialGrid
 from .regions import Region, RegionCatalog
@@ -18,4 +19,7 @@ __all__ = [
     "externalize_world_entities",
     "ColdEntityMutator",
     "ColdMutationError",
+    "MutationPrincipal",
+    "MutationDecision",
+    "MutationGate",
 ]
