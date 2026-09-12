c9344595de889b8478d9f46bd8e1dfec0b7220ab
Marcelo Roldão Matos <marceloroldao@gmail.com>
2026-09-12 02:35:01 -0300

    feat: export cold spatial storage primitives

--- packages/spatial/__init__.py
+++ packages/spatial/__init__.py
@@ -1,3 +1,4 @@
+from .cold_store import ColdRegionCandidateCache, FileRegionColdStore, externalize_world_entities
 from .region_entity_index import RegionEntityIndex
 from .region_spatial_grid import RegionLookupResult, RegionSpatialGrid
 from .regions import Region, RegionCatalog
@@ -11,4 +12,7 @@ __all__ = [
     "RegionEntityIndex",
     "RegionLookupResult",
     "RegionSpatialGrid",
+    "FileRegionColdStore",
+    "ColdRegionCandidateCache",
+    "externalize_world_entities",
 ]
